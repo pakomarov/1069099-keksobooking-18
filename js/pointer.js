cf81190b15e54c24f005960515cf66cd2c6edd57
Pavel Komarov <komarov.pa.it@gmail.com>
2019-11-16 22:06:20 +0300

    Добавляет перемещение метки

--- js/pointer.js
+++ js/pointer.js
@@ -10,12 +10,18 @@
     addressOffsetX: 32,
     addressOffsetY: 30
   };
+  var LOCATION_MIN_X = 0;
+  var LOCATION_MIN_Y = 130;
+  var LOCATION_MAX_Y = 630;
 
 
+  var mapPinsNode = document.querySelector('.map__pins');
   var pointerNode = document.querySelector('.map__pin--main');
 
 
   var appearance = APPEARANCE_BIG;
+  var initialPointerPositioning = null;
+  var locationMaxX = mapPinsNode.offsetWidth - APPEARANCE_SMALL.addressOffsetX;
   var customMouseDownHandler = function () {};
 
 
@@ -27,33 +33,89 @@
     };
   };
 
+  var refreshFormAddress = function () {
+    window.form.refreshAddress(getLocation());
+  };
+
+
+  var storeInitialPointerPositioning = function () {
+    initialPointerPositioning = window.utilities.getNodePosition(pointerNode);
+  };
+
+  var resetInitialPointerPositioning = function () {
+    pointerNode.style.left = initialPointerPositioning.left + 'px';
+    pointerNode.style.top = initialPointerPositioning.top + 'px';
+    refreshFormAddress();
+  };
+
+
   var makeBig = function () {
     appearance = APPEARANCE_BIG;
-    window.form.refreshAddress(getLocation());
+    refreshFormAddress();
   };
 
   var makeSmall = function () {
     appearance = APPEARANCE_SMALL;
-    window.form.refreshAddress(getLocation());
+    refreshFormAddress();
   };
 
-
-  var pointerMousedownHandler = function () {
-    customMouseDownHandler();
-  };
-
-  var pointerKeyDownEnterHandler = function (evt) {
-    if (evt.keyCode === window.utilities.KEYCODE_ENTER) {
-      customMouseDownHandler();
-    }
-  };
 
   var deactivate = function () {
+    resetInitialPointerPositioning();
     makeBig();
   };
 
   var activate = function () {
     makeSmall();
+  };
+
+
+  var isCoordinateXInRange = function (x) {
+    return x >= (LOCATION_MIN_X - appearance.addressOffsetX) && x <= locationMaxX;
+  };
+
+  var isCoordinateYInRange = function (y) {
+    return y >= (LOCATION_MIN_Y - appearance.addressOffsetY) && y <= (LOCATION_MAX_Y - appearance.addressOffsetY);
+  };
+
+  var pointerMousedownHandler = function (mouseDownEvent) {
+    var startCoords = {
+      x: mouseDownEvent.clientX,
+      y: mouseDownEvent.clientY
+    };
+
+    var mouseMoveHandler = function (mouseMoveEvent) {
+
+      var shift = {
+        x: startCoords.x - mouseMoveEvent.clientX,
+        y: startCoords.y - mouseMoveEvent.clientY
+      };
+
+      var newPositioning = {
+        x: pointerNode.offsetLeft - shift.x,
+        y: pointerNode.offsetTop - shift.y
+      };
+
+      if (isCoordinateXInRange(newPositioning.x)) {
+        pointerNode.style.left = newPositioning.x + 'px';
+        startCoords.x = mouseMoveEvent.clientX;
+      }
+
+      if (isCoordinateYInRange(newPositioning.y)) {
+        pointerNode.style.top = newPositioning.y + 'px';
+        startCoords.y = mouseMoveEvent.clientY;
+      }
+
+      refreshFormAddress();
+    };
+
+    var mouseUpHandler = function () {
+      document.removeEventListener('mousemove', mouseMoveHandler);
+      document.removeEventListener('mouseup', mouseUpHandler);
+    };
+
+    document.addEventListener('mousemove', mouseMoveHandler);
+    document.addEventListener('mouseup', mouseUpHandler);
   };
 
 
@@ -63,8 +125,16 @@
 
 
   var setup = function () {
+    storeInitialPointerPositioning();
+    pointerNode.addEventListener('mousedown', function () {
+      customMouseDownHandler();
+    });
+    pointerNode.addEventListener('keydown', function (evt) {
+      if (evt.keyCode === window.utilities.KEYCODE_ENTER) {
+        customMouseDownHandler();
+      }
+    });
     pointerNode.addEventListener('mousedown', pointerMousedownHandler);
-    pointerNode.addEventListener('keydown', pointerKeyDownEnterHandler);
   };
 
 
